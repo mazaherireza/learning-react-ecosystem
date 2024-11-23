7ee33a1c73bb3cb5e3f93e1b9ba1ec95ba264c19
mazaherireza <rezamazaheri.email@gmail.com>
2024-11-23 13:16:59 +0330

    React: 05- Migrate to Vite

---
+++ 01- React/05- Simple Quiz App/src/App.jsx
@@ -0,0 +1,13 @@
+import "./style/main.css";
+import "./style/App.css";
+import Quiz from "./components/quiz/Quiz.jsx";
+
+function App() {
+  return (
+    <>
+      <Quiz></Quiz>
+    </>
+  );
+}
+
+export default App;
