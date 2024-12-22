bffeb2e1be42dc17efda2e9fd00262716132272f
mazaherireza <rezamazaheri.email@gmail.com>
2024-12-22 15:11:11 +0330

    Classbon: 05- LEGP Parts - 02- Edition

--- 03- Classbon/05- LEGO Parts!/02- Call API/src/Components/Comp/comp.jsx
+++ 03- Classbon/05- LEGO Parts!/02- Call API/src/Components/Comp/comp.jsx
@@ -2,29 +2,36 @@ import axios from "../../../axios";
 import { useEffect, useState } from "react";
 
 const Comp = () => {
+  const [loading, setLoading] = useState(true);
   const [list, setList] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios("END_POINT");
       setList(data);
+      setLoading(false);
     };
     fetchData();
   }, []);
 
-  return (
-    <div className="comp-container">
-      <ul>
-        <li>
-          <a href="#">All</a>
-        </li>
+  const renderContent = () => {
+    if (loading) {
+      return <p>Loading...</p>; // <Loading /> Component
+    } else {
+      return (
+        <ul>
+          <li>
+            <a href="#">All</a>
+          </li>
 
-        {list.map((item) => {
-          const { id, title } = item;
-          return <li key={id}>{title}</li>;
-        })}
-      </ul>
-    </div>
-  );
+          {list.map((item) => {
+            const { id, title } = item;
+            return <li key={id}>{title}</li>;
+          })}
+        </ul>
+      );
+    }
+  };
+  return <div className="comp-container">{renderContent()}</div>;
 };
 
 export default Comp;
