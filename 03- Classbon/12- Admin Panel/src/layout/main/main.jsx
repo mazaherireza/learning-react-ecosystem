699db02be781a197419420c971b34939f0ef686f
mazaherireza <rezamazaheri.email@gmail.com>
2025-01-03 19:44:47 +0330

    Classbon: 12- Use Guarded Route

--- 03- Classbon/12- Admin Panel/src/layout/main/main.jsx
+++ 03- Classbon/12- Admin Panel/src/layout/main/main.jsx
@@ -1,17 +1,24 @@
 import Sidebar from "./components/sidebar/sidebar";
 import Footer from "./components/footer/footer";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./main.css";
 
 const MainLayout = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) navigate("/login");
+  }, []);
+
   return (
     <div className="main-wrapper">
       <div className="container">
         <Sidebar></Sidebar>
-        <div className="main">
+        <main className="main">
           <Outlet />
           <Footer></Footer>
-        </div>
+        </main>
       </div>
     </div>
   );
