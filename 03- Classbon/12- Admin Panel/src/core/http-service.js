5eabf01e00cba5301c1b17d5300caa2717da7a64
mazaherireza <rezamazaheri.email@gmail.com>
2025-01-04 10:14:58 +0330

    Classbon: 12- Create httpInterceptedService

--- 03- Classbon/12- Admin Panel/src/core/http-service.js
+++ 03- Classbon/12- Admin Panel/src/core/http-service.js
@@ -2,6 +2,23 @@ import axios from "axios";
 
 const BASE_URL = "https://react-mini-projects-api.classbon.com";
 
+// Used for register and login services
 export const httpService = axios.create({
   baseURL: BASE_URL,
 });
+
+export const httpInterceptedService = axios.create({
+  baseURL: BASE_URL,
+});
+
+httpInterceptedService.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token)
+      config.headers = {
+        Authorization: `Beares ${token}`,
+      };
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
