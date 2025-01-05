dfa36cd0b9adc5071ffb059a6afa13ed123c5d5f
mazaherireza <rezamazaheri.email@gmail.com>
2025-01-05 10:01:28 +0330

    Classbon: 12- Fix a Bug (Remove defer Function)

--- 03- Classbon/12- Admin Panel/src/features/courses/components/course-list.jsx
+++ 03- Classbon/12- Admin Panel/src/features/courses/components/course-list.jsx
@@ -3,7 +3,7 @@ import CourseCard from "./courseCard/course-card";
 const CourseList = ({ list }) => {
   return (
     <>
-      {list &&
+      {list.length !== 0 &&
         list.map((course) => (
           <CourseCard key={course.id} {...course}></CourseCard>
         ))}
--- 03- Classbon/12- Admin Panel/src/pages/courses/courses.jsx
+++ 03- Classbon/12- Admin Panel/src/pages/courses/courses.jsx
@@ -1,37 +1,38 @@
-import { useLoaderData, defer, Await } from "react-router-dom";
+import { useLoaderData, Await } from "react-router-dom";
 import { httpInterceptedService } from "@core/http-service";
 import CourseList from "@features/courses/components/course-list";
 import { Suspense } from "react";
 import "./courses.css";
 
 const Courses = () => {
-  const data = useLoaderData();
+  const { courses } = useLoaderData();
   return (
     <div className="courses-wrapper">
       <button>افزودن دوره‌ی جدید</button>
       <div className="grid-container">
         <Suspense fallback={<p>در حال دریافت اطلاعات</p>}>
-          <Await resolve={data.courses}>
-            {(list) => <CourseList courses={list}></CourseList>}
-          </Await>
+          <Await
+            resolve={courses}
+            children={(resolvedCourses) => (
+              <CourseList list={resolvedCourses.data}></CourseList>
+            )}
+          ></Await>
         </Suspense>
       </div>
     </div>
   );
 };
 
-const loader = async () => {
-  const { data } = await httpInterceptedService.get("/Course/list");
-  return data;
-};
-
 export const coursesLoader = async () => {
-  return defer({
-    courses: loader(),
-  });
+  const courses = httpInterceptedService.get("/Course/list");
+  return {
+    courses,
+  };
 };
 
 export default Courses;
 
-// Error:
-// react-router-dom.js does not provide an export named 'defer'
+// Await
+// Used to render promise values with automatic error handling.
+// Await expects to be renderd inside of a <Suspense>
+// <Suspense fallback={<ReviewsSkeleton />}>
