4402e0727c5c506e8da30c4c727949a05ca3a3f2
mazaherireza <rezamazaheri.email@gmail.com>
2025-01-05 11:07:15 +0330

    Classbon: 12- Dynamic Parameter - CourseDetails

---
+++ 03- Classbon/12- Admin Panel/src/features/courses/components/courseDetails/course-details.jsx
@@ -0,0 +1,14 @@
+import { httpInterceptedService } from "@core/http-service";
+import { useLoaderData } from "react-router-dom";
+
+const CourseDetails = () => {
+  const data = useLoaderData();
+  return <div className="course-details-wrapper">{data.id}</div>;
+};
+
+export const CourseDetailsLoader = async ({ params }) => {
+  const { data } = httpInterceptedService.get(`/Course/by-id/${params.id}`);
+  return data;
+};
+
+export default CourseDetails;
