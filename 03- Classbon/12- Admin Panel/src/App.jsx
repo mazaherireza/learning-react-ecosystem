238fc59cfe4a858528bc6fecf99042c263046064
mazaherireza <rezamazaheri.email@gmail.com>
2025-01-06 17:17:10 +0330

    Classbon: 12- Install React Toastify

--- 03- Classbon/12- Admin Panel/src/App.jsx
+++ 03- Classbon/12- Admin Panel/src/App.jsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+      <ToastContainer rtl></ToastContainer>
+    </>
+  );
 }
 
 export default App;
