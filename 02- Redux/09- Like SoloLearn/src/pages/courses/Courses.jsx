09aabc92d847f44eec8a13d47341bc696e1858da
mazaherireza <rezamazaheri.email@gmail.com>
2025-01-18 18:22:19 +0330

    Redux: 09- Courses - Internationalization

--- 02- Redux/09- Like SoloLearn/src/pages/courses/Courses.jsx
+++ 02- Redux/09- Like SoloLearn/src/pages/courses/Courses.jsx
@@ -1,13 +1,15 @@
 import { courses } from "@/constants/courses";
-import CourseCard from "@/components/course-card/CourseCard"
+import CourseCard from "@/components/course-card/CourseCard";
+import { useTranslation } from "react-i18next";
 import "./Courses.css";
 
 const Courses = () => {
+  const { t } = useTranslation();
   return (
     <div className="courses-wrapper">
       <div className="container">
         <div className="inner-wrapper">
-          <h2 className="page-title">Explore our courses</h2>
+          <h2 className="page-title">{t("courses.explore our courses")}</h2>
           {courses.map((course) => (
             <CourseCard key={course._id} {...course}></CourseCard>
           ))}
