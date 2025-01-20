e0fdc6d685c8871b77ea2cd0824fef01d7cdc5dd
mazaherireza <rezamazaheri.email@gmail.com>
2025-01-20 11:23:41 +0330

    09- Update Store

--- 02- Redux/09- Like SoloLearn/src/stores/courses.js
+++ 02- Redux/09- Like SoloLearn/src/stores/courses.js
@@ -1,19 +1,21 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { courses } from "../constants/courses";
 
 const NAME = "Courses";
 
-const initialState = { courses: [] };
+const initialState = { courses: courses };
 
 const coursesSlice = createSlice({
   name: `${NAME}`,
   initialState,
   reducers: {
-    addCourseAction: (state, { payload }) => {
-      state.todos.push(payload);
+    getCourseAction: (state, { payload }) => {
+      const course = state.courses.find((course) => course._id == payload);
+      return course;
     },
   },
   extraReducers: () => {},
 });
 
-export const { addCourseAction } = coursesSlice.actions;
+export const { getCourseAction } = coursesSlice.actions;
 export default coursesSlice.reducer;
