d6f9be1ea6aadc89092964fa1e8217d2a483e719
mazaherireza <rezamazaheri.email@gmail.com>
2025-07-09 09:26:13 +0330

    14: code

---
+++ 04- With Maximilian Schwatrzmüller/14- An Alternative Way Of Building Components Class-based Components/code/src/components/UseFinder.js
@@ -0,0 +1,49 @@
+import { Component, Fragment } from "react";
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: "",
+    };
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  componentDidMount() {
+    // Send HTTP request
+    this.setState({ filteredUsers: this.context.users });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm)
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
+      </Fragment>
+    );
+  }
+}
+
+export default UserFinder;
